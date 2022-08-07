5356591993fe89532f8b776879739a4e0afeb778
Its-suLav-D <dah19001@byui.edu>
2022-08-07 16:11:01 -0600

    Server and client side changes to implement the final project requirements

--- sentimentAnalyzeClient/src/App.js
+++ sentimentAnalyzeClient/src/App.js
@@ -1,8 +1,8 @@
-import './bootstrap.min.css';
-import './App.css';
-import EmotionTable from './EmotionTable.js';
-import React from 'react';
-
+import "./bootstrap.min.css";
+import "./App.css";
+import EmotionTable from "./EmotionTable.js";
+import React from "react";
+import Header from "./Header.jsx";
 class App extends React.Component {
   /*
   We are setting the component as a state named innercomp.
@@ -10,83 +10,166 @@ class App extends React.Component {
   value of the state, will be returned. The initial input mode
   is set to text
   */
-  state = {innercomp:<textarea rows="4" cols="50" id="textinput"/>,
-            mode: "text",
-          sentimentOutput:[],
-          sentiment:true
-        }
-  
+  state = {
+    innercomp: (
+      <div className="mb-3">
+        <textarea
+          className="form-control"
+          rows="3"
+          id="textinput"
+          placeholder="Enter lyrics of a song"
+        />
+        ,
+      </div>
+    ),
+    mode: "text",
+    sentimentOutput: [],
+    sentiment: true,
+  };
+
   /*
   This method returns the component based on what the input mode is.
   If the requested input mode is "text" it returns a textbox with 4 rows.
   If the requested input mode is "url" it returns a textbox with 1 row.
   */
- 
-  renderOutput = (input_mode)=>{
-    let rows = 1
-    let mode = "url"
-    //If the input mode is text make it 4 lines
-    if(input_mode === "text"){
-      mode = "text"
-      rows = 4
-    }
-      this.setState({innercomp:<textarea rows={rows} cols="50" id="textinput"/>,
-      mode: mode,
-      sentimentOutput:[],
-      sentiment:true
-      });
-  } 
-  
-  sendForSentimentAnalysis = () => {
-    this.setState({sentiment:true});
-    let url = ".";
-    let mode = this.state.mode
-    url = url+"/" + mode + "/sentiment?"+ mode + "="+document.getElementById("textinput").value;
 
-    fetch(url).then((response)=>{
-        response.json().then((data)=>{
-        this.setState({sentimentOutput:data.label});
+  renderOutput = (input_mode) => {
+    let rows = 1;
+    let mode = "url";
+    let placeholder = "Enter an url ";
+    //If the input mode is text make it 4 lines
+    if (input_mode === "text") {
+      mode = "text";
+      rows = 4;
+      placeholder = "Enter lyrics of a song";
+    }
+    this.setState({
+      innercomp: (
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            rows={rows}
+            id="textinput"
+            placeholder={placeholder}
+          />
+          ,
+        </div>
+      ),
+      mode: mode,
+      sentimentOutput: [],
+      sentiment: true,
+    });
+  };
+
+  sendForSentimentAnalysis = () => {
+    this.setState({ sentiment: true });
+    let url = ".";
+    let mode = this.state.mode;
+    url =
+      url +
+      "/" +
+      mode +
+      "/sentiment?" +
+      mode +
+      "=" +
+      document.getElementById("textinput").value;
+
+    fetch(url).then((response) => {
+      response.json().then((data) => {
+        this.setState({ sentimentOutput: data.label });
         let output = data.label;
-        let color = "white"
-        switch(output) {
-          case "positive": color = "green";break;
-          case "negative": color = "red";break;
-          default: color = "black";
+        let color = "white alert p-5 rounded";
+        switch (output) {
+          case "positive":
+            color = "alert-success";
+            break;
+          case "negative":
+            color = "alert-danger";
+            break;
+          default:
+            color = "black";
         }
-        output = <div style={{color:color,fontSize:20}}>{output}</div>
-        this.setState({sentimentOutput:output});
-      })});
-  }
+        output = (
+          <div className={color} role="alert">
+            <span className="uppercase p-4"> The result was: {output}</span>
+          </div>
+        );
+        this.setState({ sentimentOutput: output });
+      });
+    });
+  };
 
   sendForEmotionAnalysis = () => {
-
-    this.setState({sentiment:false});
+    this.setState({ sentiment: false });
     let url = ".";
-    let mode = this.state.mode
-    url = url+"/" + mode + "/emotion?"+ mode + "="+document.getElementById("textinput").value;
+    let mode = this.state.mode;
+    url =
+      url +
+      "/" +
+      mode +
+      "/emotion?" +
+      mode +
+      "=" +
+      document.getElementById("textinput").value;
 
-    fetch(url).then((response)=>{
-      response.json().then((data)=>{
-      this.setState({sentimentOutput:<EmotionTable emotions={data}/>});
-  })})  ;
-  }
-  
+    fetch(url).then((response) => {
+      response.json().then((data) => {
+        this.setState({ sentimentOutput: <EmotionTable emotions={data} /> });
+      });
+    });
+  };
 
   render() {
-    return (  
-      <div className="App">
-      <button className="btn btn-info" onClick={()=>{this.renderOutput('text')}}>Text</button>
-        <button className="btn btn-dark"  onClick={()=>{this.renderOutput('url')}}>URL</button>
-        <br/><br/>
-        {this.state.innercomp}
-        <br/>
-        <button className="btn-primary" onClick={this.sendForSentimentAnalysis}>Analyze Sentiment</button>
-        <button className="btn-primary" onClick={this.sendForEmotionAnalysis}>Analyze Emotion</button>
-        <br/>
-            {this.state.sentimentOutput}
+    return (
+      <div>
+        <Header />
+        <div className="App p-5 mb-4 bg-light rounded-3 shadow container-md">
+          <div className="button-group d-flex justify-content-center align-items-center">
+            <span className="align-center">Choose: </span>
+            <button
+              className="btn btn btn-outline-primary mx-2"
+              onClick={() => {
+                this.renderOutput("text");
+              }}
+            >
+              Text
+            </button>
+
+            <button
+              className="btn btn-outline-secondary"
+              onClick={() => {
+                this.renderOutput("url");
+              }}
+            >
+              URL
+            </button>
+          </div>
+
+          <br />
+          <br />
+          {this.state.innercomp}
+          <br />
+
+          <div className="button-group d-flex justify-content-center align-items-center">
+            <button
+              className="btn btn-outline-info mx-1"
+              onClick={this.sendForSentimentAnalysis}
+            >
+              Analyze Sentiment
+            </button>
+            <button
+              className="btn btn-secondary"
+              onClick={this.sendForEmotionAnalysis}
+            >
+              Analyze Emotion
+            </button>
+          </div>
+          <br />
+          {this.state.sentimentOutput}
+        </div>
       </div>
     );
-    }
+  }
 }
 
 export default App;
--- sentimentAnalyzeClient/src/EmotionTable.js
+++ sentimentAnalyzeClient/src/EmotionTable.js
@@ -1,31 +1,38 @@
-import React from 'react';
-import './bootstrap.min.css';
+import React from "react";
+import "./bootstrap.min.css";
 
 class EmotionTable extends React.Component {
-    render() {
-      //Returns the emotions as an HTML table
-      return (  
-        <div>
-          <table className="table table-bordered">
-            <tbody>
+  render() {
+    //Returns the emotions as an HTML table
+    return (
+      <div>
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th scope="col">SN.</th>
+              <th scope="col">Emotion</th>
+              <th scope="col">Score</th>
+            </tr>
+          </thead>
+          <tbody>
             {
               /*Write code to use the .map method that you worked on in the 
               Hands-on React lab to extract the emotions. If you are stuck,
               please click the instructions to see how to implement a map*/
-              Object.entries(this.props.emotions).map(emotion=> {
-                  return (
-                      <tr>
-                          <td>{emotion[0]}</td> 
-                          <td>{emotion[1]}</td> 
-                          </tr>
-                  )
+              Object.entries(this.props.emotions).map((emotion, idx) => {
+                return (
+                  <tr key={idx}>
+                    <th scope="row">{idx}</th>
+                    <td>{emotion[0]}</td>
+                    <td>{emotion[1]}</td>
+                  </tr>
+                );
               })
             }
-            </tbody>
-          </table>
-          </div>
-          );
-        }
-    
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
 export default EmotionTable;
